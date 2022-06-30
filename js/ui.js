d9d866d863a68efa7b21b412ab04c6b70fda76ee
NotImplementedLife <not.implemented.life@gmail.com>
2022-06-30 23:19:30 +0300

    reworked footer

--- js/ui.js
+++ js/ui.js
@@ -1,27 +1,22 @@
 const footer =
 `<div class="jumbotron" style="margin:0; margin-top:15vh; min-height:50vh">		
-	<table style="width:100%; table-layout: fixed;">						
-		<tr>
-			<th></th>
-			<th><h4>Contact</h4></th>
-			<th><h4>Also on</h4></th>
-		</tr>
-		<tr>
-			<td></td>
-			<td>Discord (NotImpLife#1049)</td>				
-			<td><a href="https://notimplementedlife.itch.io">itch.io</a></td>				
-		</tr>
-		<tr>
-			<td></td>
-			<td><a href="https://twitter.com/NotImpLife">Twitter</a></td>
-			<td><a href="https://www.youtube.com/channel/UC-0C58sSNynr5kTgT24NXqQ">Youtube</a></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td><a href="https://github.com/NotImplementedLife">GitHub</a></td>
-		</tr>
-	</table>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-3">
+				<h4>Contact</h4>
+				Discord (NotImpLife#1049)<br/>
+				<a href="https://notimplementedlife.itch.io">itch.io</a><br/>
+				<a href="https://twitter.com/NotImpLife">Twitter</a><br/>
+				
+			</div>
+			<div class="col-md-3">
+				<h4>Also on</h4>
+				<a href="https://github.com/NotImplementedLife">GitHub</a><br/>
+				<a href="https://www.youtube.com/channel/UC-0C58sSNynr5kTgT24NXqQ">Youtube</a><br/>				
+			</div>
+		</div>
+	</div>	
 </div>`;
 
 
