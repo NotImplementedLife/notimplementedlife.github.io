d1a181b9ec9dab4323312f66e01bd53a19b9d697
NotImplementedLife <not.implemented.life@gmail.com>
2022-07-01 12:10:15 +0300

    updated footer

--- js/ui.js
+++ js/ui.js
@@ -5,8 +5,7 @@ const footer =
 			<div class="col-md-3"></div>
 			<div class="col-md-3">
 				<h4>Contact</h4>
-				Discord (NotImpLife#1049)<br/>
-				<a href="https://notimplementedlife.itch.io">itch.io</a><br/>
+				Discord (NotImpLife#1049)<br/>				
 				<a href="https://twitter.com/NotImpLife">Twitter</a><br/>
 				
 			</div>
@@ -14,6 +13,7 @@ const footer =
 				<h4>Also on</h4>
 				<a href="https://gbatemp.net/members/notimplife.553721/">GBATemp</a><br/>				
 				<a href="https://github.com/NotImplementedLife">GitHub</a><br/>				
+				<a href="https://notimplementedlife.itch.io">itch.io</a><br/>
 				<a href="https://www.youtube.com/channel/UC-0C58sSNynr5kTgT24NXqQ">Youtube</a><br/>				
 			</div>
 		</div>
