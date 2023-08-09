4840f825715350615228d62a119c196ca9370bc3
NotImplementedLife <not.implemented.life@gmail.com>
2023-08-09 19:41:16 +0300

    fixed search bar on dev page

--- dev/js/ui.js
+++ dev/js/ui.js
@@ -1,20 +1,21 @@
 const footer =
-`<div class="jumbotron" style="margin:0; margin-top:15vh; min-height:50vh">		
+`<div class="jumbotron" style="margin:0; margin-top:15vh; min-height:50vh;" id="footer">		
 	<div class="container">
 		<div class="row">
-			<div class="col-md-3"></div>
-			<div class="col-md-3">
+			<div class="col-md-1"></div>
+			<div class="col-md-5">
 				<h4>Contact</h4>
-				Discord (NotImpLife#1049)<br/>				
-				<a href="https://twitter.com/NotImpLife">Twitter</a><br/>
-				
+				<a href="https://discord.com/users/792512288769703938" class="fa fa-discord" style="color:white"> NotImpLife#1049</a><br/>
+				<a href="https://twitter.com/NotImpLife" class="fa fa-twitter" style="color:white"> NotImpLife</a><br/>
+				<a rel="me" href="https://retrochat.online/@NotImplementedLife" class="fa fa-mastodon" style="color:white"> @NotImplementedLife@retrochat.online</a><br/>
+				<a href="mailto: not.implemented.life@gmail.com" class="fa fa-google" style="color:white"> not.implemented.life@gmail.com</a><br/>
 			</div>
-			<div class="col-md-3">
-				<h4>Also on</h4>				
-				<a href="https://gbatemp.net/members/notimplife.553721/">GBATemp</a><br/>				
-				<a href="https://github.com/NotImplementedLife">GitHub</a><br/>
-				<a href="https://notimplementedlife.itch.io">itch.io</a><br/>
-				<a href="https://www.youtube.com/channel/UC-0C58sSNynr5kTgT24NXqQ">Youtube</a><br/>				
+			<div class="col-md-4">
+				<h4>Also on</h4>
+				<a href="https://gbatemp.net/members/notimplife.553721/" style="color:white"> GBATemp</a><br/>				
+				<a href="https://github.com/NotImplementedLife" class="fa fa-github" style="color:white"> GitHub</a><br/>				
+				<a href="https://notimplementedlife.itch.io" class="fa fa-itch-io" style="color:white"> itch.io</a><br/>
+				<a href="https://www.youtube.com/channel/UC-0C58sSNynr5kTgT24NXqQ" class="fa fa-youtube" style="color:white"> Youtube</a><br/>								
 			</div>
 		</div>
 	</div>	
